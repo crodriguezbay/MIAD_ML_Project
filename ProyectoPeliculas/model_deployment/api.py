9b437e62bcf783aacde73e963a0a38e077d243cf
Wendy Barreda <wendybarredac@gmail.com>
2022-05-28 10:33:15 -0500

    pretty format

--- ProyectoPeliculas/model_deployment/api.py
+++ ProyectoPeliculas/model_deployment/api.py
@@ -40,9 +40,32 @@ class GenderApi(Resource):
         app.logger.info(args)
 
         json_object = json.loads(predict_proba(args['Plot']))
-        
+
         return {
-         'result' : { 'p_Action' : json_object['p_Action']
+         'result' : {'p_Action' : json_object['p_Action'],
+                    'p_Adventure' : json_object['p_Adventure'], 
+                    'p_Animation' : json_object['p_Animation'], 
+                    'p_Biography' : json_object['p_Biography'], 
+                    'p_Comedy' : json_object['p_Comedy'], 
+                    'p_Crime' : json_object['p_Crime'], 
+                    'p_Documentary' : json_object['p_Documentary'], 
+                    'p_Drama' : json_object['p_Drama'], 
+                    'p_Family' : json_object['p_Family'],
+                    'p_Fantasy' : json_object['p_Fantasy'], 
+                    'p_Film-Noir' : json_object['p_Film-Noir'], 
+                    'p_History' : json_object['p_History'], 
+                    'p_Horror' : json_object['p_Horror'], 
+                    'p_Music' : json_object['p_Music'], 
+                    'p_Musical' : json_object['p_Musical'], 
+                    'p_Mystery' : json_object['p_Mystery'], 
+                    'p_News' : json_object['p_News'], 
+                    'p_Romance' : json_object['p_Romance'],
+                    'p_Sci-Fi' : json_object['p_Sci-Fi'], 
+                    'p_Short' : json_object['p_Short'], 
+                    'p_Sport' : json_object['p_Sport'], 
+                    'p_Thriller' : json_object['p_Thriller'], 
+                    'p_War' : json_object['p_War'], 
+                    'p_Western' : json_object['p_Western']
          }
         }, 200
     
